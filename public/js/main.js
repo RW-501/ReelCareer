273ed6813d0aacbc7bc37a8e6b7ada595f03ae87
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-04 10:48:54 -0500

    update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,22 @@
 
 */
 
+// main.js
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.googletagmanager.com/gtag/js?id=G-LBTK319K2X','ga');
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-LBTK319K2X');
+
+
 // Bootstrap CSS 
-import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+//import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
