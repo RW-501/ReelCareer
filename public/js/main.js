22dcb28bb4f43c736f77b0584dd49ea5595d066c
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-05 18:34:36 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -356,7 +356,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to create the navbar
     function createNavbar() {
-        const navbarClass = currentPage === '/index.html' ?  'navbar-dark bg-dark' : 'navbar-light bg-light';
+        const navbarClass = (isHomePage) ?  'navbar-dark bg-dark' : 'navbar-light bg-light';
         
         console.log("nav currentPage   ",currentPage);
         console.log("nav isHomePage   ",isHomePage);
