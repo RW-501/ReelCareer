7d236d7de7e358a320ad6ce4dd2120c3ec73facf
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-05 22:41:08 -0500

    views

--- public/js/main.js
+++ public/js/main.js
@@ -481,7 +481,7 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
 
             document.getElementById('loginButton').onclick = () => {
-                window.location.href = adjustLinkHomeURL+'public/auth.html'; // Redirect to login page
+                window.location.href = adjustLinkURL+'views/auth.html'; // Redirect to login page
             };
         }
     }
@@ -490,7 +490,7 @@ document.addEventListener("DOMContentLoaded", function () {
     async function logoutUser() {
         try {
             await firebase.auth().signOut();
-            window.location.href =  adjustLinkHomeURL+'public/auth.html'; // Redirect to login page after logout
+            window.location.href =  adjustLinkURL+'views/auth.html'; // Redirect to login page after logout
         } catch (error) {
             console.error("Logout error:", error);
         }
