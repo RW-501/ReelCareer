a025abe11e948f83d4c9099bda6ce26dd9a32b1a
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-07 21:09:32 -0500

    up

--- public/js/main.js
+++ public/js/main.js
@@ -418,7 +418,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const faviconStandard = document.createElement('link');
             faviconStandard.rel = 'icon';
             faviconStandard.type = 'image/x-icon';
-            faviconStandard.href = adjustLinkURL+'images/favicons/favicon.ico';
+            faviconStandard.href = adjustLinkHomeURL+'images/favicons/favicon.ico';
             head.appendChild(faviconStandard);
         
             // 32x32 Favicon
@@ -426,7 +426,7 @@ document.addEventListener("DOMContentLoaded", function () {
             favicon32.rel = 'icon';
             favicon32.type = 'image/png';
             favicon32.sizes = '32x32';
-            favicon32.href = adjustLinkURL+'images/favicons/favicon-32x32.png';
+            favicon32.href = adjustLinkHomeURL+'images/favicons/favicon-32x32.png';
             head.appendChild(favicon32);
         
             // 16x16 Favicon
@@ -434,14 +434,14 @@ document.addEventListener("DOMContentLoaded", function () {
             favicon16.rel = 'icon';
             favicon16.type = 'image/png';
             favicon16.sizes = '16x16';
-            favicon16.href = adjustLinkURL+'images/favicons/favicon-16x16.png';
+            favicon16.href = adjustLinkHomeURL+'images/favicons/favicon-16x16.png';
             head.appendChild(favicon16);
         
             // Apple Touch Icon
             const appleTouchIcon = document.createElement('link');
             appleTouchIcon.rel = 'apple-touch-icon';
             appleTouchIcon.sizes = '180x180';
-            appleTouchIcon.href = adjustLinkURL+'images/favicons/apple-touch-icon.png';
+            appleTouchIcon.href = adjustLinkHomeURL+'images/favicons/apple-touch-icon.png';
             head.appendChild(appleTouchIcon);
         
             // Android Favicon (192x192)
@@ -449,7 +449,7 @@ document.addEventListener("DOMContentLoaded", function () {
             androidFavicon192.rel = 'icon';
             androidFavicon192.type = 'image/png';
             androidFavicon192.sizes = '192x192';
-            androidFavicon192.href = adjustLinkURL+'images/favicons/android-chrome-192x192.png';
+            androidFavicon192.href = adjustLinkHomeURL+'images/favicons/android-chrome-192x192.png';
             head.appendChild(androidFavicon192);
         
             // Chrome Favicon (512x512)
@@ -457,7 +457,7 @@ document.addEventListener("DOMContentLoaded", function () {
             androidFavicon512.rel = 'icon';
             androidFavicon512.type = 'image/png';
             androidFavicon512.sizes = '512x512';
-            androidFavicon512.href = adjustLinkURL+'images/favicons/android-chrome-512x512.png';
+            androidFavicon512.href = adjustLinkHomeURL+'images/favicons/android-chrome-512x512.png';
             head.appendChild(androidFavicon512);
         }
         
