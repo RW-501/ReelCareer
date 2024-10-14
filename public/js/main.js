5f1fc211232c2509dff63c1553916d713809e0f0
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-13 23:46:19 -0500

    up

--- public/js/main.js
+++ public/js/main.js
@@ -1012,7 +1012,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const profileForm = document.getElementById('profileForm');
                 if (profileForm) {
                     console.log('User info????????????/  :', userData);
-                    console.log("verified: ", verified);
+                    console.log("userINFO: ", userINFO);
 
                     populateFormFields(user); // Populate fields with user data
                     document.getElementById('settingsBtn').addEventListener('click', showModal);
