c6034b5cb020928e4a4d1e21102f382faf146b54
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-15 19:10:33 -0500

    up

--- public/js/bodyload.js
+++ public/js/bodyload.js
@@ -190,8 +190,8 @@ function addStyles() {
 
 /* Loading Dots Animation */
 @keyframes loadingText {
-    0%, 100% { opacity: 0.3; }
-    50% { opacity: 1; }
+    0%, 100% { opacity: 0.1; }
+    50% { opacity: 0.5; }
     100% { opacity: 1; }
 }
 
