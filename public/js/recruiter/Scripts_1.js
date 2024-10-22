31259fef5758f5c24474121317fef7332b7e0337
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-10-22 10:50:57 -0500

    up

--- public/js/recruiter/Scripts_1.js
+++ public/js/recruiter/Scripts_1.js
@@ -385,7 +385,8 @@ function addMoreMultipleChoice(multipleChoiceDiv, questionNumber) {
     
         // Append the new input and remove button to the multiple choice div
         multipleChoiceDiv.appendChild(choiceInput);
-    multipleChoiceDiv.insertBefore(removeButton, multipleChoiceDiv.lastElementChild);
+        multipleChoiceDiv.appendChild(removeButton);
+   // multipleChoiceDiv.insertBefore(removeButton, multipleChoiceDiv.lastElementChild);
 }
 
 
