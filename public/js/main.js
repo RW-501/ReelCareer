51667d9a5c34f94887fb55566d8f8f3f065e0880
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-11-24 05:22:30 -0600

    kkkbhh

--- public/js/main.js
+++ public/js/main.js
@@ -2268,3 +2268,108 @@ function triggerUpdateWithTimeout(ipAddress, time) {
 
 
 
+
+// Function to check if the user is logged in
+function checkUserLoginStatus() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      console.log('User is logged in:', user);
+      return user;
+    } else {
+      // No user is signed in
+      console.log('No user is logged in.');
+      return false;
+    }
+  });
+
+}
+
+window.checkUserLoginStatus = checkUserLoginStatus;
+
+
+// Logout function
+function logout() {    // Clear auto logout timer
+  clearTimeout(autoLogoutTimer);
+  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem('autoLogoutTime');
+  showToast('You have been logged out.');
+  // Redirect to login or home page
+  window.location.href = '../';
+}
+
+
+window.logout = logout;
+
+
+// Check if user is logged in and handle admin area access
+function checkLogin() {
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
+  // Redirect to home if user is not logged in and is in the admin area
+  if (window.location.pathname.includes('/admin/')) {
+      if (!isLoggedIn) {
+          if (window.location.pathname.includes('/admin/index')) {
+              showToast('You need to log in to access the Admin area.');
+          } else {
+              // Redirect to login page or main admin page
+              window.location.href = '/admin/index';
+          }
+      } else if (window.location.pathname.includes('/admin') || window.location.pathname.includes('/admin/index') || window.location.pathname.includes('/admin/')) {
+          showToast('Admin Logged In');
+          let firebaseLogin = document.getElementById("firebaseLogin");
+          let dashboardContent = document.getElementById("dashboardContent");
+      
+          if(firebaseLogin){
+              firebaseLogin = firebaseLogin.style.display = "none";
+              dashboardContent = dashboardContent.style.display = "block";
+
+          }
+      
+      }
+  }
+}
+
+window.checkLogin = checkLogin;
+
+let autoLogoutTimer = null;
+
+// Initialize auto logout on page load
+function initializeAutoLogout() {
+  const savedMinutes = localStorage.getItem('autoLogoutTime');
+  if (savedMinutes && !isNaN(savedMinutes)) {
+      // Set the timer using the saved setting
+      autoLogoutTimer = setTimeout(() => {
+          logout();
+      }, parseInt(savedMinutes) * 60 * 1000);
+
+
+      showToast(`Auto logout initialized for ${savedMinutes} minutes.`);
+  } else {
+     // showToast('Auto logout is disabled.');
+  }
+}
+
+window.initializeAutoLogout = initializeAutoLogout;
+
+
+
+// Initialization
+document.addEventListener('DOMContentLoaded', () => {
+  checkLogin(); // Ensure login is valid on page load
+
+    
+if (window.checkUrl("/backend/") || window.checkUrl("/backend")) {
+  console.log("Admin View");
+  checkUserLoginStatus();
+  initializeAutoLogout();
+} else {
+  console.log("User View");
+  attachTrackingListeners();
+}
+
+
+});
+
+
+
