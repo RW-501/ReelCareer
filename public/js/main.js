f93f217d0e5d4e1572f96b1679a8f6c339d273a0
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-01 22:23:21 -0600

    bfz

--- public/js/main.js
+++ public/js/main.js
@@ -118,11 +118,12 @@ const userIP = sessionStorage.getItem('userIP') || "";
     await setDoc(userDocRef, userData, { merge: true });
 
     
+    localStorage.setItem('userTagInterest', JSON.stringify(userData));
 
     localStorage.setItem('userJobInterest', JSON.stringify(userJobInterest));
     localStorage.setItem('userTagInterest', JSON.stringify(userTagInterest));
 
-    localStorage.setItem("userData", userData);
+
     localStorage.setItem("userLoggedIn", "true");
 
 
