82794898f01fea9f88de7dc26cd663f4eb678b2f
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-07 00:17:04 -0600

    up

--- public/js/bodyload.js
+++ public/js/bodyload.js
@@ -253,6 +253,9 @@ createLoader("ReelCareer");
   // Function to create a job card and append it to the container
   function createJobCard(job, container) {
     // Create the job card
+
+    console.log("Job Data:", job);
+
     const jobCard = document.createElement("div");
     jobCard.id = `job_Card_${job.id}`;
     jobCard.className = "JOB_CARD  mb-4 noCopy";
