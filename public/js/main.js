0832ac0f6a8542059196f60dda09854a5e569d8b
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-09 23:37:03 -0600

    zbfbb

--- public/js/main.js
+++ public/js/main.js
@@ -2661,12 +2661,11 @@ document.body.appendChild(chatPanel);
     }
   }
   
-  // Attach resize event listener
-  window.addEventListener("resize", resizeChatPanel);
-  window.addEventListener("DOMContentLoaded", () => {
+
+    // Attach resize event listener
+    window.addEventListener("resize", resizeChatPanel);
     resizeChatPanel(); // Call initially on load
     replaceMainContentWithBot(); // Replace content if on /bot page
-  });
   
   // Event Listeners
   chatButton.addEventListener("click", () => {
