d61f29d2515c0e4d8d7d602db0e4f99c655d1a8a
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-11 21:34:45 -0600

    uo

--- bot/js/brain.js
+++ bot/js/brain.js
@@ -538,20 +538,21 @@
                 'mourning', 'bereaved', 'devastated', 'heartbroken', 'sorrowful', 'heavy', 'weeping', 'downcast', 'empty', 'mournful', 
                 'bereft', 'desolate', 'lonely', 'grieving', 'abandoned'
             ],
-             states: {
-                "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", 
-                "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", 
-                "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", 
-                "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", 
-                "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", 
-                "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", 
-                "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", 
-                "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", 
-                "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", 
-                "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", 
-                "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", 
-                "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming"
+            states: {
+                "AL": "alabama", "AK": "alaska", "AZ": "arizona", "AR": "arkansas", 
+                "CA": "california", "CO": "colorado", "CT": "connecticut", "DE": "delaware", 
+                "FL": "florida", "GA": "georgia", "HI": "hawaii", "ID": "idaho", "IL": "illinois", 
+                "IN": "indiana", "IA": "iowa", "KS": "kansas", "KY": "kentucky", "LA": "louisiana", 
+                "ME": "maine", "MD": "maryland", "MA": "massachusetts", "MI": "michigan", 
+                "MN": "minnesota", "MS": "mississippi", "MO": "missouri", "MT": "montana", 
+                "NE": "nebraska", "NV": "nevada", "NH": "new hampshire", "NJ": "new jersey", 
+                "NM": "new mexico", "NY": "new york", "NC": "north carolina", "ND": "north dakota", 
+                "OH": "ohio", "OK": "oklahoma", "OR": "oregon", "PA": "pennsylvania", "RI": "rhode island", 
+                "SC": "south carolina", "SD": "south dakota", "TN": "tennessee", "TX": "texas", 
+                "UT": "utah", "VT": "vermont", "VA": "virginia", "WA": "washington", 
+                "WV": "west virginia", "WI": "wisconsin", "WY": "wyoming"
             },
+            
             money: ['money', 'income', 'wage', 'earnings', 'cost', 'revenue', 'expenses', 'budget', 'finance', 'financial'],
 
             compare: ['like', 'same','better', 'worse', 'higher', 'lower', 'greater', 'less', 'equal', 'more', 'fewer', 'increase', 'decrease'],
