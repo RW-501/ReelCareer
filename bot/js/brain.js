b24672500a04535b3d3b06a39460e9970fec4b5f
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-12 04:34:50 -0600

    up

--- bot/js/brain.js
+++ bot/js/brain.js
@@ -998,6 +998,23 @@ async function handleJobQuery( tokens,categorizedTokens, userPreferences) {
 
 
 
+import {
+    db, getStorage, ref, uploadBytes, getDownloadURL, limit,
+  doc, arrayUnion, RecaptchaVerifier, increment, getDoc, arrayRemove, signInWithPhoneNumber,
+  query, updateDoc, setDoc, addDoc, signInAnonymously, orderBy, onAuthStateChanged,
+  uploadBytesResumable, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, startAfter,
+  OAuthProvider, signOut, deleteDoc, getFirestore, serverTimestamp,
+  createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteObject,
+  where, getDocs, storage, getAuth, collection, auth, analytics, 
+  googleProvider,onSnapshot ,  batch,
+  facebookProvider,writeBatch ,
+  getUserId // Export the function
+  } from 'https://reelcareer.co/js/module.js';
+  
+  
+  
+
+
 // **Helper Functions for Fetching Job Data:**
 
 async function fetchJobData({ location, jobType }) {
