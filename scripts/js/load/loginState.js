48d9cdb493283bcb978a4246cf9361aea0ea2bfd
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-12-21 15:46:26 -0600

    up

--- scripts/js/load/loginState.js
+++ scripts/js/load/loginState.js
@@ -1,4 +1,21 @@
 
+import {
+  db, getStorage, ref, uploadBytes, getDownloadURL, limit,
+  doc, arrayUnion, RecaptchaVerifier, increment, getDoc, arrayRemove, signInWithPhoneNumber,
+  query, updateDoc, setDoc, addDoc, signInAnonymously, orderBy, onAuthStateChanged,
+  uploadBytesResumable, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, startAfter,
+  OAuthProvider, signOut, deleteDoc, getFirestore, serverTimestamp,
+  createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteObject,
+  where, getDocs, storage, getAuth, collection, auth, analytics,
+  googleProvider,onSnapshot ,writeBatch ,batch,
+  facebookProvider,
+  getUserId
+} from 'https://reelcareer.co/scripts/js/load/module.js';
+
+
+
+
+
 
 // Function to update or create user information in Firestore
 const saveUserLoginState = async (user) => {
