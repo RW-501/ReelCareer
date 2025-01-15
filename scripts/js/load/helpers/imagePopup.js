a2e5deae10ece57961bddd63cc99f3ac1de8844f
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-01-14 18:23:15 -0600

    up

--- scripts/js/load/helpers/imagePopup.js
+++ scripts/js/load/helpers/imagePopup.js
@@ -1,23 +1,29 @@
 
-document.addEventListener("DOMContentLoaded", () => {
     // Add click listener to lazy-image elements
     document.body.addEventListener("click", event => {
         const target = event.target;
+          // Check if the clicked element is a lazy-image
+          if (target.classList.contains("video-post")) {
+            const imageSrc = target.src || target.getAttribute("data-videosrc");  
+
+            const profileID = target.getAttribute("data-created-by-i-d");
+            // Create the full-screen popup
+            createImagePopup(imageSrc, profileID);
+        }
+ 
   
         // Check if the clicked element is a lazy-image
         if (target.classList.contains("lazy-image")) {
             const imageSrc = target.src || target.getAttribute("data-src");  
-             const mediaId = target.getAttribute("data-id");
-  
-
-
+            
+            const mediaId = target.getAttribute("data-id");
             // Create the full-screen popup
             createImagePopup(imageSrc);
         }
     });
   
     // Function to create the full-screen popup
-    const createImagePopup = (imageSrc) => {
+    const createImagePopup = (imageSrc, idURL) => {
         // Create the overlay
         const overlay = document.createElement("div");
         overlay.classList.add("fullscreen-popup");
@@ -39,10 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
         // Add functionality to the "More Videos" button
         const moreVideosButton = overlay.querySelector(".more-images-button");
         moreVideosButton.addEventListener("click", () => {
-          window.location.href = `/u/?u=${mediaId}`;
+          window.location.href = `/u/?u=${idURL}`;
          //   alert("Redirect to videos or perform another action here.");
         });
     };
-  });
-  
+
+    
   
