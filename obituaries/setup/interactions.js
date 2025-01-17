0a860dbe54e5c562fca7553f2f949d22a78695b3
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-01-17 10:04:07 -0600

    up

--- obituaries/setup/interactions.js
+++ obituaries/setup/interactions.js
@@ -19,8 +19,11 @@ function renderInteractionsArea(firstName) {
   <div>
     <input type="text" id="guestName" placeholder="Your Name" required>
   </div>
+            <div class="flex-wrap">
       <input type="checkbox" id="anonymousCheckbox">
     <label for="anonymousCheckbox">Submit as Anonymous</label>
+  </div>
+
   </div>
 
   <div>
