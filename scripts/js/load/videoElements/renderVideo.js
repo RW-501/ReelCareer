3d4e104e8b67cece653563a8b4ebfacfcc7c8709
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-01-19 16:13:47 -0600

    ui

--- scripts/js/load/videoElements/renderVideo.js
+++ scripts/js/load/videoElements/renderVideo.js
@@ -1,5 +1,18 @@
 
 
+import {
+  db, getStorage, ref, uploadBytes, getDownloadURL, limit,
+doc, arrayUnion, RecaptchaVerifier, increment, getDoc, arrayRemove, signInWithPhoneNumber,
+query, updateDoc, setDoc, addDoc, signInAnonymously, orderBy, onAuthStateChanged,
+uploadBytesResumable, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, startAfter,
+OAuthProvider, signOut, deleteDoc, getFirestore, serverTimestamp,
+createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteObject,
+where, getDocs, storage, getAuth, collection, auth, analytics,
+googleProvider,onSnapshot ,
+facebookProvider,
+getUserId // Export the function
+} from 'https://reelcareer.co/scripts/js/load/module.js';
+
 
 function renderVideos(docs, container, connectedUserIds, userId) {
   
