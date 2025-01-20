2e1d9e5f5008bec9884f1d09d2d5186fb71ab940
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-01-19 22:48:54 -0600

    jkb

--- scripts/js/load/videoElements/sideNav.js
+++ scripts/js/load/videoElements/sideNav.js
@@ -396,14 +396,16 @@ function insertSidePanelContent() {
     }
 
 
-
-
 .side-nav-button.active {
-  background-color: #1a73e8;  /* Example Google-style blue color */
-  color: white;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      border: #89bdda solid;
+      box-shadow: 0 2px 4px rgba(0, 123, 255, 0.3); /* Subtle shadow for depth */
 }
 
+
+.embedded-Log {
+padding: 1rem;
+margins: auto;
+}
 
 .side-panel-content {
     display: grid;
