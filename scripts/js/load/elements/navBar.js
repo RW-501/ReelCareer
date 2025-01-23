db7c778a4dc1118fcc14b9df191ac676620d6116
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-01-22 21:19:20 -0600

    ,,,

--- scripts/js/load/elements/navBar.js
+++ scripts/js/load/elements/navBar.js
@@ -162,7 +162,8 @@ const userDataSaved = getUserData() || {};
 
 const lastUpdateTimestamp = userDataSaved.lastUpdateTime;
 const lastUpdateDate = new Date((lastUpdateTimestamp.seconds * 1000) + (lastUpdateTimestamp.nanoseconds / 1000000));
-//console.log("handleAuthStateChanged lastUpdateTime:", lastUpdateDate);
+
+console.log("lastUpdateTime:", lastUpdateDate);
 
 if (!lastUpdateDate || (new Date() - lastUpdateDate) > 30 * 60 * 1000) {
   console.log("updating user:", user.displayName);
