fab4b24a8dc642194096ebec72d3bc1241a09ec6
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-08-09 19:15:55 -0500

    up

--- scripts/js/load/loginState.js
+++ scripts/js/load/loginState.js
@@ -4,7 +4,7 @@ import {
   doc, arrayUnion, RecaptchaVerifier, increment, getDoc, arrayRemove, signInWithPhoneNumber,
   query, updateDoc, setDoc, addDoc, signInAnonymously, orderBy, onAuthStateChanged,
   uploadBytesResumable, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, startAfter,
-  OAuthProvider, signOut, deleteDoc, getFirestore, serverTimestamp,
+  OAuthProvider, signOut, deleteDoc, serverTimestamp,
   createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteObject,
   where, getDocs, storage, getAuth, collection, auth, analytics,
   googleProvider, onSnapshot, writeBatch, batch,
